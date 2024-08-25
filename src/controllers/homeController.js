36578ff81c811043123572e5b12a0dbf375a224f
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-08-26 00:28:57 +0700

    Fourth commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,7 +3,7 @@ import CRUDservice from '../services/CRUDservice';
 
 let getHomePage = async (req, res) => {
     try {
-        
+
         let data = await db.User.findAll();
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
@@ -32,9 +32,47 @@ let postCRUD = async (req, res) => {
     return res.send('post crud from server');
 }
 
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDservice.getAllUser();
+    console.log('----------------------------')
+    console.log(data)
+    console.log('----------------------------')
+
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    })
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    console.log(userId)
+    if (userId) {
+        let userData = await CRUDservice.getUserInfoById(userId);
+        // check user data not found
+        // let userData
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    }
+    else {
+        return res.send('Users not found!');
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDservice.updateUserData(data); 
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    })
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
--- src/services/CRUDservice.js
+++ src/services/CRUDservice.js
@@ -1,6 +1,7 @@
 
 import bcrypt from 'bcryptjs';
 import db from '../models/index';
+import { raw } from 'body-parser';
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -39,6 +40,65 @@ let hashUserPassword = (password) => {
     })
 }
 
+let getAllUser = () => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            let users = db.User.findAll({
+                raw: true,
+            });
+            resolve(users)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let getUserInfoById = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId },
+                raw: true,
+            })
+
+            if(user){
+                resolve(user)
+            }
+            else{
+                resolve({})
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let updateUserData = (data) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: data.id }
+            })
+            if(user){
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+
+                await user.save();
+                let allUsers = await db.User.findAll();
+                resolve(allUsers);
+            } else {
+                resolve();
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    })
+}
+
 module.exports = {
-    createNewUser: createNewUser
+    createNewUser: createNewUser,
+    getAllUser: getAllUser,
+    getUserInfoById: getUserInfoById,
+    updateUserData: updateUserData,
 }
