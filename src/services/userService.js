76590e4550985159eaa633616272651ad5cba66f
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-08-28 00:37:01 +0700

    Sixth commit

---
+++ src/services/userService.js
@@ -0,0 +1,69 @@
+import { where } from "sequelize";
+import db from "../models/index";
+import bcrypt, { hash } from 'bcryptjs';
+import { raw } from "body-parser";
+
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {};
+
+            let isExist = await checkUserEmail(email);
+            if(isExist) {
+                //user already exist
+
+                let user = await db.User.findOne({
+                    attributes: ['email', 'roleId', 'password'],
+                    where: {email: email},
+                    raw: true,
+                });
+                if(user){
+                    //compare password
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if(check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'OK';
+                        
+                        delete user.password;
+                        userData.user = user;
+                    } else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'Wrong password';
+                    }
+                } else {
+                    userData.errCode = 2;
+                    userData.errMessage = `User's isn't not found~ `
+                }
+            } else {
+                // return error
+                userData.errCode = 1;
+                userData.errMessage = "Your's Email isn't exist in your system. Plz try other email!" 
+            }
+            resolve(userData)
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail}
+            })
+            if(user){
+                resolve(true)
+            } else {
+                resolve(false )
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+module.exports = {
+    handleUserLogin: handleUserLogin,
+}
