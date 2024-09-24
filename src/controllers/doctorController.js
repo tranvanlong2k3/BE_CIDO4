8eca5573b2572d32e214d7ec68e36c4ba97c0d6e
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-09-24 20:14:32 +0700

    Eighteenth commit

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -4,7 +4,7 @@ let getTopDoctorHome = async (req, res) => {
     let limit = req.query.limit;
     if (!limit) limit = 10;
     try {
-        let response = await doctorService.getTopDoctorHome(limit);
+        let response = await doctorService.getTopDoctorHome(+limit);
         return res.status(200).json(response);
     } catch (e) {
         console.log(e);
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,16 +1,23 @@
 import { raw } from "body-parser";
 import db from "../models/index";
+import { where } from "sequelize";
 
 let getTopDoctorHome = (limitInput) => {
     return new Promise(async (resolve, reject) => {
         try {
             let users = await db.User.findAll({
                 limit: limitInput,
+                where: { roleId: 'R2' },
                 order: [['createdAt', 'DESC']],
                 attributes:{
                     exclude: ['password']
                 },
-                // raw: true
+                include: [
+                    { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+                    { model: db.Allcode, as: 'genderData', attributes: ['valueEn', 'valueVi'] }
+                ],
+                raw: true,
+                nest: true
             })
 
             resolve({
