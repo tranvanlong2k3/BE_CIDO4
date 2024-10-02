f320f7c4c6215c386c01b3e4531860c2ff4f1499
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-10-03 00:08:07 +0700

    Twenty-seventh commit

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,3 +1,4 @@
+import { where } from "sequelize";
 import db from "../models/index";
 require('dotenv').config();
 import _ from 'lodash';
@@ -56,13 +57,21 @@ let getAllDoctors = () => {
 let saveDetailInforDoctor = (inputData) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!inputData.doctorId || !inputData.contentHTML
-                || !inputData.contentMarkdown || !inputData.action) {
+            if (!inputData.doctorId 
+                || !inputData.contentHTML
+                || !inputData.contentMarkdown || !inputData.action 
+                || !inputData.selectedPrice || !inputData.selectedPayment 
+                || !inputData.selectedProvince 
+                || !inputData.nameClinic || !inputData.addressClinic 
+                || !inputData.note
+                ) {
                 resolve({
                     errCode: 1,
                     errMessage: 'Missing parameter'
                 })
             } else {
+
+                //Upsert to Markdown
                 if (inputData.action === 'CREATE') {
                     await db.Markdown.create({
                         contentHTML: inputData.contentHTML,
@@ -80,11 +89,41 @@ let saveDetailInforDoctor = (inputData) => {
                         doctorMarkdown.contentMarkdown = inputData.contentMarkdown;
                         doctorMarkdown.description = inputData.description;
                         doctorMarkdown.updateAt = new Date();
-
                         await doctorMarkdown.save();
                     }
                 }
 
+                //Upsert to Doctor_infor table
+                let doctorInfor = await db.Doctor_Infor.findOne({
+                    where: {
+                        doctorId: inputData.doctorId,
+                    },
+                    raw: false
+                })
+
+                if(doctorInfor){
+                    //update
+                    doctorInfor.doctorId = inputData.doctorId;
+                    doctorInfor.priceId = inputData.selectedPrice;
+                    doctorInfor.paymentId = inputData.selectedPayment;
+                    doctorInfor.provinceId = inputData.selectedProvince;
+                    doctorInfor.nameClinic = inputData.nameClinic;
+                    doctorInfor.addressClinic = inputData.addressClinic;
+                    doctorInfor.note = inputData.note;
+
+                    await doctorInfor.save();
+                }else{
+                    //create
+                    await db.Doctor_Infor.create({
+                        doctorId: inputData.doctorId,
+                        priceId : inputData.selectedPrice,
+                        paymentId : inputData.selectedPayment,
+                        provinceId : inputData.selectedProvince,
+                        nameClinic : inputData.nameClinic,
+                        addressClinic : inputData.addressClinic,
+                        note : inputData.note,
+                    })
+                }
 
                 resolve({
                     errCode: 0,
