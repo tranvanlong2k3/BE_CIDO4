47eab20f4e0ea33f4b1dd5f49d76f1965895639f
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-10-06 02:34:36 +0700

    Thirty-sixth commit

--- src/services/specialtyService.js
+++ src/services/specialtyService.js
@@ -1,5 +1,6 @@
 const { name } = require("body-parser");
 const db = require("../models");
+const { reject } = require("bcrypt/promises");
 
 let createSpecialty = (data) => {
     return new  Promise(async (resolve, reject) => {
@@ -33,6 +34,30 @@ let createSpecialty = (data) => {
     })
 }
 
+let getAllSpecialty = () => {
+    return new Promise(async ( resolve, reject) => {
+        try {
+            let data = await db.Specialty.findAll({
+
+            });
+            if(data && data.length > 0) {
+                data.map(item => {
+                    item.image = new Buffer(item.image, 'base64').toString('binary');
+                    return item;
+                })
+            }
+            resolve({
+                errMessage: 'Ok',
+                errCode: 0,
+                data
+            })
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
-    createSpecialty: createSpecialty
+    createSpecialty: createSpecialty,
+    getAllSpecialty: getAllSpecialty
 }
