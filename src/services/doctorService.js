def4d91b73ff611a8ab32f6db0793760bc5958bd
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2024-10-06 03:19:01 +0700

    Thirty-seventh commit

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,7 +1,7 @@
 import { where } from "sequelize";
 import db from "../models/index";
 require('dotenv').config();
-import _, { reject } from 'lodash';
+import _ from 'lodash';
 
 const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 
@@ -54,20 +54,36 @@ let getAllDoctors = () => {
     })
 }
 
+let checkRequiredFields = (inputData) => {
+    let arrFields = ['doctorId', 'contentHTML', 'contentMarkdown', 'action',
+        'selectedPrice', 'selectedPayment', 'selectedProvince', 'nameClinic',
+        'addressClinic', 'note', 'specialtyId'
+    ]
+
+    let isValid = true;
+    let element = '';
+    for (let i = 0; i < arrFields.length; i++) {
+        if(!inputData[arrFields[i]]) {
+            isValid = false;
+            element = arrFields[i]
+            break;
+        }
+    }
+
+    return {
+        isValid: isValid,
+        element: element
+    }
+}
+
 let saveDetailInforDoctor = (inputData) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!inputData.doctorId 
-                || !inputData.contentHTML
-                || !inputData.contentMarkdown || !inputData.action 
-                || !inputData.selectedPrice || !inputData.selectedPayment 
-                || !inputData.selectedProvince 
-                || !inputData.nameClinic || !inputData.addressClinic 
-                || !inputData.note
-                ) {
+            let checkObj = checkRequiredFields(inputData);
+            if (checkObj.isValid === false) {
                 resolve({
                     errCode: 1,
-                    errMessage: 'Missing parameter'
+                    errMessage: `Missing parameter: ${checkObj.element}`
                 })
             } else {
 
@@ -110,6 +126,8 @@ let saveDetailInforDoctor = (inputData) => {
                     doctorInfor.nameClinic = inputData.nameClinic;
                     doctorInfor.addressClinic = inputData.addressClinic;
                     doctorInfor.note = inputData.note;
+                    doctorInfor.specialtyId = inputData.specialtyId;
+                    doctorInfor.clinicId = inputData.clinicId;
 
                     await doctorInfor.save();
                 }else{
@@ -122,6 +140,8 @@ let saveDetailInforDoctor = (inputData) => {
                         nameClinic : inputData.nameClinic,
                         addressClinic : inputData.addressClinic,
                         note : inputData.note,
+                        specialtyId : inputData.specialtyId,
+                        clinicId : inputData.clinicId,
                     })
                 }
 
